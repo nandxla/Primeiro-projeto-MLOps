f63875099de273a7bd44d6c94dce5f1bbb0f2e37
Fernando Santos <s4antosf3rnando@gmail.com>
2023-10-30 18:54:51 -0300

    finalizando pagina predict

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ import numpy as np
 
 app = Flask(__name__)
 
+
 with open('cls_iris.pkl', 'rb') as file:
     model = pickle.load(file)
 
@@ -14,15 +15,22 @@ def home():
 
 @app.route('/predict', methods=['POST'])
 def predict():
+    dict_convert = {
+    0: "setosa",
+    1: "versicolor",
+    2: "virginica",
+}
+
     sepal_len = request.form.get('sepal_len')
     sepal_wid = request.form.get('sepal_wid')
     petal_len = request.form.get('petal_len')
     petal_wid = request.form.get('petal_wid')
     
-    datas = np.array([sepal_len, sepal_wid, petal_len, petal_wid]).astype('float')  # transformando os dados em uma lista numpy
+    datas = np.array([petal_len, petal_wid, sepal_len, sepal_wid]).astype('float')  # transformando os dados em uma lista numpy
     pred = model.predict(datas.reshape(1, -1))
     
-    return render_template('predict.html', pred=pred[0])
+    return render_template('predict.html', pred=dict_convert[pred[0]])
+
 
 if __name__ == '__main__':
     app.run(debug=True)
